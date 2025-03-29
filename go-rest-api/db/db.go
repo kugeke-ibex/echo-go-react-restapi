537e4e99d5a06295c329314df0d8d102233ce1e8
Kenjiro Kuge <kugetyan0211@gmail.com>
2025-03-29 07:12:46 +0000

    feat: GORM DBファイル 作成

---
+++ go-rest-api/db/db.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"log"
+	"os"
+	"fmt"
+
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+)
+
+func NewDB() *gorm.DB {
+	if os.Getenv("GO_ENV") == "dev" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	"postgress://{user}:{password}@{host}:{port}/{database}"
+	db, err := gorm.Open(postgres.Open(os.Getenv("POSTGRES_URL")), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Connected")
+	return db
+}
+
+func CloseDB(db *gorm.DB) {
+	sqlDB, _ := db.DB()
+	if err := sqlDB.Close(); err != nil {
+		log.Fatalln(err)
+	}
+}
